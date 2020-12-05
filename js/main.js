ae83601aa1197fdfc68dbf611d4d97dfb3319eaa
JessicaCastoldi <74505237+JessicaCastoldi@users.noreply.github.com>
2020-12-05 14:45:03 -0300

    Meu primeiro commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Jessica Castoldi";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japao é o melhor time do mundo"
+    //alert(nome + " tem " + idade + "anos");
+    //alert(idade + idade2);
+console.log(frase);
+console.log(nome);
+console.log(idade2);
+console.log(frase.replace("Japao", "Brasil"));
+alert(frase.replace("Japao", "Brasil"));
+console.log(frase.toLocaleLowerCase());
+console.log(frase.toUpperCase());
